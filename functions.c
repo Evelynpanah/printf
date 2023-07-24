42dcc1648fe2382ba1af432031d5d57bbcb570a8
Evelynpanah <evelynmwaramba@gmail.com>
2023-07-24 01:30:43 -0700

    Updated

--- functions.c
+++ functions.c
@@ -40,24 +40,6 @@ int print_string(va_list types, char buffer[],
         if (str == NULL)
         {
                 str = "(null)";
-                if (precision >= 6)
-                        str = "      ";
-        }
-        while (str[length] != '\0')
-                length++;
-        if (precision >= 0 && precision < length)
-                length = precision;
-        if (width > length)
-        {
-                if (flags & F_MINUS)
-                {
-                        write(1, &str[0], length);
-                        for (i = width - length; i > 0; i--)
-                                write(1, " ", 1);
-                        return (width);
-                }
-                else
-                {
                         for (i = width - length; i > 0; i--)
                                 write(1, " ", 1);
                         write(1, &str[0], length);
@@ -150,20 +132,3 @@ int print_binary(va_list types, char buffer[],
         m = 2147483648; /* (2 ^ 31) */
         a[0] = n / m;
         for (i = 1; i < 32; i++)
-        {
-                m /= 2;
-                a[i] = (n / m) % 2;
-        }
-        for (i = 0, sum = 0, count = 0; i < 32; i++)
-        {
-                sum += a[i];
-                if (sum || i == 31)
-                {
-                        char z = '0' + a[i];
-                        write(1, &z, 1);
-                        count++;
-                }
-        }
-        return (count);
-}
-
